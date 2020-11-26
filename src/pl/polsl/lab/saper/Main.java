e71b2be80240bb80261aa857e7c1e3ca7ee6c093
Konrad Skrzypczyk <44729545+skrzypak@users.noreply.github.com>
2020-11-26 11:32:36 +0100

    Fix focus bug

--- src/pl/polsl/lab/saper/Main.java
+++ src/pl/polsl/lab/saper/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Game.fxml"));
-        primaryStage.setTitle("Minesweeper game");
+        primaryStage.setTitle("Minesweeper");
         primaryStage.setScene(new Scene(root, 650, 450));
         primaryStage.show();
     }
