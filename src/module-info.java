910f8a30d2a72742f20f4a5d642dc30cb0e1edf7
Konrad Skrzypczyk <44729545+skrzypak@users.noreply.github.com>
2020-11-26 11:03:04 +0100

    Configuration

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module Saper {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires jdk.jfr;
+    requires org.junit.jupiter.api;
+    requires org.junit.jupiter.params;
+    opens pl.polsl.lab.saper;
+}
